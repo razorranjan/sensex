fd43a6f5c4480c1acf3ecfb42868a6e1558b9fdd
ranjan <ranjan.s@theluxurycloset.com>
2019-07-13 19:26:45 +0530

    Updates

---
+++ precache-manifest.20d6261a8d27b4d0f765e9bb87ea0767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a58f409a7a766a55b19f81d2d9c3e83",
+    "url": "/sensex/index.html"
+  },
+  {
+    "revision": "23af15df505df038abac",
+    "url": "/sensex/static/css/main.280a9f29.chunk.css"
+  },
+  {
+    "revision": "de51b8daa84f0711742f",
+    "url": "/sensex/static/js/2.87779fe4.chunk.js"
+  },
+  {
+    "revision": "23af15df505df038abac",
+    "url": "/sensex/static/js/main.d5d76d1d.chunk.js"
+  },
+  {
+    "revision": "92f23a8e7d3172ea7e10",
+    "url": "/sensex/static/js/runtime~main.22c52f38.js"
+  }
+]);
